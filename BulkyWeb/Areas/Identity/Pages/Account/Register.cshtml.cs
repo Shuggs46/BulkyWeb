d7ff3719b82fdb20efc6d5d5b393080d3362139f
Shuggs <michael@formatech-it.com>
2024-03-25 09:56:32 -0700

    119. Assign Roles on Registration - Completed

--- BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -50,18 +50,15 @@ public class RegisterModel : PageModel
             _emailStore = GetEmailStore();
             _signInManager = signInManager;
             _logger = logger;
-            _emailSender = emailSender;
-           
+            _emailSender = emailSender;         
         }
 
 
-
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [BindProperty]
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    [BindProperty]
     public InputModel Input { get; set; }
 
     /// <summary>
