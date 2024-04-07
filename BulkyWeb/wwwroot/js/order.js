ac3c07676512acd77fef2517a15153c4a0990c4d
Shuggs <michael@formatech-it.com>
2024-04-07 10:56:39 -0700

    158. Order List UI - Incomplete - updated

--- BulkyWeb/wwwroot/js/order.js
+++ BulkyWeb/wwwroot/js/order.js
@@ -9,12 +9,12 @@ function loadDataTable() {
         "ajax": { url: '/admin/order/getall' },
         "columns":
         [
-        { data: 'id',"width": "25%" },
-        { data: 'name',"width": "15%" },
-        { data: 'phonenumber',"width": "10%" },
-        { data: 'applicationuser.email',"width": "15%" },
-        { data: 'orderstatus', "width": "10%" },
-        { data: 'ordertotal', "width": "10%" },
+                { data: 'id', "width": "5%" },
+                            { data: 'name', "width": "25%" },
+                            { data: 'phoneNumber', "width": "20%" },
+                            { data: 'applicationUser.email', "width": "20%" },
+                            { data: 'orderStatus', "width": "10%" },
+                            { data: 'orderTotal', "width": "10%" },
                 {
                     data: 'id',
                     "render": function (data) {
